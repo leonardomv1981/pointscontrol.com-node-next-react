9c20a1b9436174e51f90708f693496de9a60d5b8
Leonardo M Vilela <99840717+leonardomv1981@users.noreply.github.com>
2026-05-11 18:10:01 -0300

    finalize status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+  let version = await database.query("SHOW server_version;");
+  version = version.rows[0].server_version;
+  let maxConnections = await database.query("SHOW max_connections;");
+  maxConnections = maxConnections.rows[0].max_connections;
+  let usedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  usedConnections = usedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: parseInt(maxConnections),
+        used_connections: usedConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  // console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const version = responseBody.dependencies.database.version;
+  expect(version).toEqual("16.13");
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.used_connections).toEqual(1);
+
+  // const usedConnections = responseBody.used_connections;
+  // expect(usedConnections).toEqual(0);
 });
