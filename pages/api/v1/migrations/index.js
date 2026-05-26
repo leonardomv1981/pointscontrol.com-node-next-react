42da9429592b67515ddacce4028873a5e5abf400
Leonardo M Vilela <99840717+leonardomv1981@users.noreply.github.com>
2026-05-26 20:31:44 -0300

    correction to end database and return 405 if method was not post or get on

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -26,6 +26,9 @@ export default async function migrations(request, response) {
       return response.status(201).json(migratedMigrations);
     }
     return response.status(200).json(migratedMigrations);
+  } else {
+    await dbClient.end();
+    return response.status(405).end();
   }
   return response.status(405).end();
 }
