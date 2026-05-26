70a8c1fbc3df1ac2d3ae4c5517be866a6b6c9a8d
Leonardo M Vilela <99840717+leonardomv1981@users.noreply.github.com>
2026-05-26 17:04:36 -0300

    refactor database

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,31 @@
+// import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: "infra/migrations",
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  } else if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  }
+  return response.status(405).end();
+}
