8285da1b092348bdfd494b525f6cd4d93262c32f
Leonardo M Vilela <99840717+leonardomv1981@users.noreply.github.com>
2026-08-05 17:45:59 -0300

    fix: insert `process.cws()` at endpoint of migrations to prevent error

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -18,7 +18,7 @@ export default async function migrations(request, response) {
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: resolve("infra", "migrations"),
+      dir: resolve(process.cwd(), "infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
