eade665004440ed78dc9dee80ca870f8db1942c0
Leonardo M Vilela <99840717+leonardomv1981@users.noreply.github.com>
2026-08-17 18:51:22 -0300

    refactor: refactor model migrations

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,22 +5,23 @@ async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    return retry(fetchStatusPages, {
+    return retry(fetchStatusPage, {
       retries: 100,
       maxTimeout: 1000,
     });
 
-    async function fetchStatusPages() {
-      const response = await fetch("http://localhost:3000/api/v1/migrations");
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+
       if (response.status !== 200) {
-        throw new Error();
+        throw Error();
       }
     }
   }
 }
 
 async function clearDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+  await database.query("drop schema public cascade; create schema public;");
 }
 
 const orchestrator = {
